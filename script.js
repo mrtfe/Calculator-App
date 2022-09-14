1576361acd8b560ca3f524f4b734e898a52bbe53
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-14 20:01:29 +0300

    feat: basic functions added

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Script is loaded");
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
+
+function operate(operator, num1, num2) {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "-") {
+    multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  } else console.log("error");
+}
+
+console.log(operate("/", 5, 9));
