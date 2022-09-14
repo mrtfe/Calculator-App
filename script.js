abb96a49b811bba0a389e632f755dfc3fd4bfdae
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-14 20:59:21 +0300

    feat: CLEAR and DEL buttons functions added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//buttons
+const CLEAR_BTN = document.getElementById("clear");
+const DEL_BTN = document.getElementById("del");
+const NUM_BTN = document.querySelectorAll(".button-num");
+const OPERATOR_BTN = document.querySelectorAll(".button-operator");
+const EQUAL_BTN = document.getElementById("equal");
+//display
+const DISPLAY = document.querySelector(".display");
+
+CLEAR_BTN.addEventListener("click", () => {
+  DISPLAY.innerHTML = "0";
+});
+DEL_BTN.addEventListener("click", () => {
+  DISPLAY.textContent = DISPLAY.textContent.toString().slice(0, -1);
+});
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -32,3 +48,7 @@ function operate(operator, num1, num2) {
 }
 
 console.log(operate("/", 5, 9));
+
+function addToDisplay(element) {
+  DISPLAY.innerHTML = DISPLAY += element.value;
+}
