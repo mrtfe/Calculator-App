03263deda1f999ed72e00fcf6db8f7641b2c70f6
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-19 20:58:23 +0300

    feat: buttons working, add values to display. Some styles added.

--- script.js
+++ script.js
@@ -4,14 +4,43 @@ const DEL_BTN = document.getElementById("del");
 const NUM_BTN = document.querySelectorAll(".button-num");
 const OPERATOR_BTN = document.querySelectorAll(".button-operator");
 const EQUAL_BTN = document.getElementById("equal");
+const CURRENT_OPERAND = document.querySelector(".current-operand");
+const PREVIOUS_OPERAND = document.querySelector(".previous-operand");
+
 //display
 const DISPLAY = document.querySelector(".display");
 
+//buttons event listeners
 CLEAR_BTN.addEventListener("click", () => {
-  DISPLAY.innerHTML = "0";
+  CURRENT_OPERAND.innerHTML = null;
+  PREVIOUS_OPERAND.innerHTML = null;
 });
 DEL_BTN.addEventListener("click", () => {
-  DISPLAY.textContent = DISPLAY.textContent.toString().slice(0, -1);
+  CURRENT_OPERAND.textContent = CURRENT_OPERAND.textContent
+    .toString()
+    .slice(0, -1);
+});
+
+NUM_BTN.forEach((button) =>
+  button.addEventListener("click", () => {
+    CURRENT_OPERAND.innerHTML += button.textContent;
+  })
+);
+
+function clearOperand() {
+  CURRENT_OPERAND.innerHTML = null;
+}
+
+OPERATOR_BTN.forEach((button) => {
+  button.addEventListener("click", () => {
+    CURRENT_OPERAND.innerHTML += button.textContent;
+    PREVIOUS_OPERAND.innerHTML = CURRENT_OPERAND.textContent;
+    clearOperand();
+  });
+});
+
+EQUAL_BTN.addEventListener("click", () => {
+  operate(operator, num1, num2);
 });
 
 function add(num1, num2) {
@@ -29,11 +58,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
-
-console.log(add(10, 5));
-console.log(subtract(10, 5));
-console.log(multiply(10, 5));
-console.log(divide(10, 5));
 
 function operate(operator, num1, num2) {
   if (operator == "+") {
