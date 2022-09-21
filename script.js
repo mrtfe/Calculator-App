448ce028caf937568a71b50de13e54a1fb450d7a
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-21 20:07:38 +0300

    multiply function bug

--- script.js
+++ script.js
@@ -57,18 +57,22 @@ EQUAL_BTN.addEventListener("click", () => {
 });
 
 function add(num1, num2) {
+  console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator}`);
   return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2) {
+  console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator}`);
   return parseInt(num1) - parseInt(num2);
 }
 
 function multiply(num1, num2) {
+  console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator}`);
   return parseInt(num1) * parseInt(num2);
 }
 
 function divide(num1, num2) {
+  console.log(`num1: ${num1}, num2: ${num2}, operator: ${operator}`);
   return parseInt(num1) / parseInt(num2);
 }
 
